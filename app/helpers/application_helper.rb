3c57b852d340c9091a3f4ce6f1ad1d80b0d2ba46
Jeroen Jagt <jeroenpjagt@gmail.com>
2018-09-26 20:45:37 +0200

    made line href more consistent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def get_garment_path(garment)
     garment_path(garment.category.name, garment.path)
   end
+
+  def count_slashes(path)
+    return 0 if path == "/"
+    path.scan(/\//).count
+  end
+
+  def line_to(*args, &block)
+    current_slashes = count_slashes(request.env['PATH_INFO'])
+    link_slashes = count_slashes(args[1])
+    klass = %(line #{:back if link_slashes < current_slashes})
+
+    args[2] = {} if args[2].nil?
+    args[2][:class] = %(#{args[2][:class]} #{klass})
+
+    link_to(*args, &block)
+  end
 end
