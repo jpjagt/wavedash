e9ae49dcd109706bc530a5656e26d8b808078f2d
Jeroen Jagt <jeroenpjagt@gmail.com>
2018-11-20 00:42:21 +0000

    set order record for all users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_order
+
+  private
+
+  def set_order
+    begin
+      @order = Order.find(session[:order_id])
+    rescue ActiveRecord::RecordNotFound
+      # create a new order
+      @order = Order.create
+      session[:order_id] = @order.id
+    end
+  end
 end
