1b1c1ce0e121b4c0d0aa32b04384a8d10ed34e3f
Jeroen Jagt <jeroenpjagt@gmail.com>
2020-07-24 11:49:09 +0200

    add bg transition

--- app/javascript/controllers/cube_controller.js
+++ app/javascript/controllers/cube_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from "stimulus"
 
 import { createCubeKeyframes } from '../cubemath'
+import { createElementFromHTML } from '../utils'
 
-const animationDuration = 1200
+const animationDuration = 1400
 
 export default class extends Controller {
   static targets = [ 'cubeContainer', 'frontSide' ]
@@ -18,32 +19,41 @@ export default class extends Controller {
 
   onRender (event) {
     if (window.previousFrontSide) {
-      this.rotateToNewContent(window.previousFrontSide)
+      this.transitionToNewContent(window.previousFrontSide, window.previousBackground)
 
       window.previousFrontSide = null
     }
   }
 
   onBeforeVisit (event) {
+    window.previousBackground = this.getCubeBackground().outerHTML.toString()
     window.previousFrontSide = this.getFrontSide().innerHTML.toString()
   }
 
   getCubeContainer = () => document.querySelector('#cube-container')
   getFrontSide = () => document.querySelector('#cube .front .side-inner')
+  getCubeBackground = () => document.querySelector('#cube-container .cube-bg')
 
-  rotateToNewContent (oldContent) {
+  transitionToNewContent (oldContent, oldBackground) {
     const frontSide = this.getFrontSide()
     if (frontSide) {
       const content = frontSide.innerHTML
       frontSide.innerHTML = oldContent
       setTimeout(() => {
         this.rotate()
+        this.transitionBackground(oldBackground)
       }, 10)
 
       setTimeout(() => {
         frontSide.innerHTML = content
       }, animationDuration / 2)
     }
+  }
+
+  transitionBackground (oldBackground) {
+    const bgElem = createElementFromHTML(oldBackground)
+    this.getCubeContainer().appendChild(bgElem)
+    bgElem.classList.add('disappearing')
   }
 
   rotate () {
